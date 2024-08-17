4b55a24d53358d5af2f1e17c6f79655fcbf66c93
designer-ab <per.useravi0@gmail.com>
2024-08-17 12:04:16 +0600

    added banner

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,7 +1,9 @@
+import banner from "../assets/banner/banner.png"
+
 const Home = () => {
     return (
-        <div>
-            <p>home</p>
+        <div className="mt-10">
+            <img className="rounded-lg" src={banner} alt="" />
         </div>
     );
 };
